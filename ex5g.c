4691192f5a33c85d029b6c5344aa7338802c9720
Ashish Das <62236340+Sugar3201@users.noreply.github.com>
2021-12-13 22:01:27 +0530

    Add files via upload

---
+++ ex5g.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<omp.h>
+
+int main(void)
+{
+  int sum = 0, n;
+  
+  printf("Enter how many values you want to insert in the array: ");
+  scanf("%d",&n);
+  
+  int arr[n];
+  
+  printf("\nEnter the values in the array: \n");
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",&arr[i]);
+  }
+  
+  #pragma omp parallel for schedule(static, 3) firstprivate(sum)
+  for(int i = 0; i < 2; i++)
+  {
+    if(i == 0)
+    {
+      for(int j=0; j<n; j++)
+      {
+        if(arr[j]%2 == 0)
+        {
+          sum = sum + arr[j];
+        }
+      }
+      printf("\nSum of Even Numbers: %d",sum);
+    }
+    else{
+      for(int j=0; j<n; j++)
+      {
+        if(arr[j]%2 != 0)
+        {
+          sum = sum + arr[j];
+        }
+      }
+      printf("\nSum of Odd Numbers: %d",sum);
+    }
+  }
+}
